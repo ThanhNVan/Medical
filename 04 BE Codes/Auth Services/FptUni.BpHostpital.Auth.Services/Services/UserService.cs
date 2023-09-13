f13a9bd986de7ab173e341be3e9c3c3001b5fe3b
ThanhNVan <thanh.van@wave5.nl>
2023-09-13 23:44:46 +0700

    fix jwt with

--- 04 BE Codes/Auth Services/FptUni.BpHostpital.Auth.Services/Services/UserService.cs
+++ 04 BE Codes/Auth Services/FptUni.BpHostpital.Auth.Services/Services/UserService.cs
@@ -256,6 +256,7 @@ public class UserService : IUserService
                 new Claim(ClaimTypes.Name, user.Fullname),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("UserId", user.Id),
 
                 //roles
                 new Claim(ClaimTypes.Role, rolesClaims)
