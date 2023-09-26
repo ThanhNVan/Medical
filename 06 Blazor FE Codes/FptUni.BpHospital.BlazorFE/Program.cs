3ebb9f89fa049d2a54121061c5a92c5c74d04bb6
ThanhNVan <thanh.van@wave5.nl>
2023-09-26 17:55:36 +0700

    added blazor FE

---
+++ 06 Blazor FE Codes/FptUni.BpHospital.BlazorFE/Program.cs
@@ -0,0 +1,43 @@
+using FptUni.BpHospital.BlazorFE.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
+namespace FptUni.BpHospital.BlazorFE;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddSyncfusionBlazor();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+        var app = builder.Build();
+        SyncfusionLicenseProvider.RegisterLicense(builder.Configuration["SyncfusionKey"]);
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
+}
